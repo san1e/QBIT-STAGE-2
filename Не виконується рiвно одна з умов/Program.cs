072cb283989d0524592b121f5259d48903916ce6
Олександр Клименко <alklimenko227@gmail.com>
2022-10-28 17:10:25 +0300

    Створення проекту "Не виконується рiвно одна з умов"

---
+++ Не виконується рiвно одна з умов/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Не_виконується_рiвно_одна_з_умов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
